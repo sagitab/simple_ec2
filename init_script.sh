6d2bf61a427a4ddba5929e0924f80d4f4a1532f9
sagitab <sagilavy01@gmail.com>
2026-01-05 14:28:45 +0200

    init

---
+++ init_script.sh
@@ -0,0 +1,18 @@
+# Update the package database
+sudo yum update -y
+
+# Install Docker
+sudo yum install -y docker
+
+# Start the Docker service
+sudo service docker start
+
+# Enable Docker to start on boot
+sudo systemctl enable docker
+
+# Add the ec2-user to the docker group
+sudo usermod -a -G docker ec2-user
+
+newgrp docker
+
+docker ps
